745fe72fe5b272c5e2a7806511f89a9da1b4ff6e
Christian Smith <tuk96703@temple.edu>
2022-10-06 23:06:32 -0400

    Full functional code and finished ReadME + Part A answer
    This should be the last or one of the last commits before submission.

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
-# PhilliesDataProcessing
- This is for the Phillies SWE Internship/Associate position
+**Steps on how to get my results**
 
- We need to process the top 125 salaries in the team, then take that average and set that as the qualifying offer for a departing free agent player
-
-When a team signs a free agent they give up a draft pick.
-
-We use this data set: https://questionnaire-148920.appspot.com/swe/data.html
-
-The data changes slightly each time you reload the page this is ensuring the code is dynamic and properly parses the data new and old.
-
-Display any other relevant data the the user regarding finding that qualifying offer price: (EX: names?, highest current salary?, USD(currency)?, etc..)
-
-There will be data without certain fields or corrupted or malformed names. Ignore missing values but make sure they don't effect the average. 
-
-Make sure code is readable, presented nicely, and use comments to describe your functionality.
-
-Also make sure to add a ReadME.md to explain how to get the results I get.
+***Result should something like this***
+*The price for the upcoming qualifying offer is: 16544994.14*
 
 Step #1: Make sure to install BeautifulSoup via typing "pip install BeautifulSoup4" on your terminal line
 Issue: This was incredibly finiky for me due to me installing python via windows shop, this is good for using VSCode however it causes issues with the library being used globally which caused certain errors for me.
 
-Step #2: Make sure you are either scrapping the info from the webpage or the html file
+Step #2: Download the dataProcessing.py, philliesDataWebPage.html (if you want to parse from file), and a way to run python code (via IDE or terminal with python installed) or you can download the entire github repo as a zipped folder with the link I provided.
 
-Step #3: Future christian this is for you to explain how it gets the average salary
+Step #3: Make sure you are either scrapping the info from the webpage or the html file (there is a command present in code to use from file, just uncomment the source = filereader, and comment out the source = bs(html.parser)) |this is done near lines 8 and 9|.
 
-Step #4: Explain how to use you got the average they want you to return
+Step #4: This is not really a step more rather me telling you that the finalResult variable is the actual float with qualifying offer price, what is being printed is actually just a formatted string for better presentation. |this is in reference to lines 109 and 131|.
+
+Note: My answer to part A of this coding challening is in this repo as a .txt file
+
+Here is the answer for part A just in case:
+
+A. The code I would use to validate if the string is a palindrome is this:
+
+class Solution(object):
+    def isPalindrome(self, s):
+        s = ''.join(e for e in s if e.isalnum()).lower() #making it so string s is replacing all non letter char's into nothing then lowercasing the ones that are letter chars
+        return s==s[::-1] #[::-1] starts at the end of a string and moves back, this is returning the BOOL of if S from start the finish, and S from finish to start = eachother
+
+This code is an improvement in my opinion because it is shorter and more scalable. We are using library functions from the Python language and on top of that we make use of Python
+functionality with the return statement. Since the algorithm will be used or copied and rarely modified for it's task I find a shorter implementation is always better, and a programmer
+should always take advantage of a languages functionality. This runtime is also faster, however we do sacrifice space-time complexitiy here since we use .isalnum() and .lower(). If we
+wanted to not waste space we would implement a version using a left and right pointer and not build a string to check, rather check the characters themselves.
--- dataProcessing.py
+++ dataProcessing.py
@@ -5,13 +5,9 @@ from dataclasses import dataclass
 
 def main():
     #when we test offline use source from file not weblink
-    #source = urllib.request.urlopen('https://questionnaire-148920.appspot.com/swe/data.html').read() #we get the link
-    source = open("philliesDataWebPage.html", "r").read() #turns into string
+    source = urllib.request.urlopen('https://questionnaire-148920.appspot.com/swe/data.html').read() #we get the link
+    #source = open("philliesDataWebPage.html", "r").read() #turns into string
     soup = bs.BeautifulSoup(source,'html.parser') #how we set the html and it is saved into a string
-    
-    #we are creating count variables to make sure we don't ruin our calculations from our parsing function
-    countOfPlayers = 0
-    countOfSalaries = 0
 
     #function here to parse soup so that we can extract the data
     def parseNames(data):
@@ -53,16 +49,91 @@ def main():
     playerYears = parseYear(soup)
     playerLevels = parseLevel(soup)
 
+    #created the list of players that holds their parsed data such as names, salary, year, level
     playersList = []
 
-    #loop to put things into a list of players
+    #loop to put player objects with all parameters into a list of players
     for i in range(len(playerNames)):
         playersList.append(players(playerNames[i], playerSalaries[i], playerYears[i], playerLevels[i]))
 
-    '''for player in playersList: TESTING!!!
-        print(player)'''
+    #method that turns a list of string salaries into a list of integer salaries and removes key characters + puts a '-1' for when a salaries was corrupted/or empty
+    def salStringsToInts(listOfSal): #turning the list of salaries into a list of int salaries
+            resultList = []
+            for sal in listOfSal:
+                if(sal == ""): #edge case for corrupted data #1
+                    sal = -1
+                
+                if(sal != -1 and sal != "no salary data"): #edge case for corrupted data #2
+                    sal = sal.strip("$")
+                    sal = sal.replace(",","")
+                    sal = int(sal)
+                else: #clearing all further edge cases
+                    sal = -1
+                resultList.append(sal)
+            return resultList
 
-    #print(playersList[0].name) #can print the name of whatever player in the list per index you can also do .salary , .year, .level
+    salaryList= []
+    for i in range(len(playersList)): #fill up salaryList
+        salaryList.append(playersList[i].salary)
+    
+    intSalaryList = salStringsToInts(salaryList) #calling this method on the list of salary strings to create list of salary int's
+    for i in range(len(playersList)):
+        playersList[i].salary = intSalaryList[i] #filling playersList salaries with correct integer data for future use
+
+    #method to take a list of int(salaries) and return a sorted version greatest->least
+    def findTop125Salaries(intSalaryList):
+        topSalaries = [] #setting a new list to contain top salaries
+        count = 0 #this is to help us figure out when we have reached 125 salaries in topSalaries
+        intSalaryList.sort() #sort the list in ascending order
+        intSalaryList.reverse() #reverse the sorted list so the highest salaries are first
+        for salary in intSalaryList:
+            if(count == 125):
+                return topSalaries
+            else:
+                topSalaries.append(salary)
+                count+=1
+
+    topSalaries = findTop125Salaries(intSalaryList) #we call findTop125Salaries to create and set a variable to a list of top 125 salaries
+
+    #method to calculate the qualifying salary
+    def calculateQualifyingSal(topSalaries):
+        qualOfferPrice = 0
+        sum = 0
+        #calculate qualifying offer price
+        for sal in topSalaries:
+            if(sal != -1): #this is to make sure we aren't calculating our -1 error check into our qualOfferPrice
+                sum += sal
+        qualOfferPrice = (sum/125) #math to find the average
+        return qualOfferPrice
+
+    finalResult = calculateQualifyingSal(topSalaries) #this is your actual variable that holds the correct float data of the qualifying offer price
+
+    #find a way to print result out to stdout with proper formatting (included correct decimals)
+    finalResult = str(finalResult) #turn into string
+    qualifyingOfferPrice = "" #create new string to build
+    checkStr = "" #create new string to build just the 2 decimal #'s
+    decFlag = 0 #flag to see if we passed a '.' char
+    for c in finalResult: #loop to get rid of extra decimals
+        if(decFlag == 0):
+            qualifyingOfferPrice += c
+        if(decFlag == 1):
+            checkStr += c
+        if(c == '.'):
+            decFlag = 1
+        if(len(checkStr) == 2):
+            qualifyingOfferPrice += checkStr
+
+    if(len(checkStr) == 1): #extra check to make sure we put a zero if there is only one decimal present
+        qualifyingOfferPrice += checkStr
+        qualifyingOfferPrice += "0"
+
+    #here we print qualifyingOfferPrice which is a string version of finalResult, we do this to present the price with the appropiate decimals
+    print("The price for the upcoming qualifying offer is:", qualifyingOfferPrice)
 
 if __name__ == "__main__": #running our main
     main()
+    '''
+        we parse data with "no salary data" , "multiple $$$", no ','s
+        and empty string values for salary correctly
+    '''
+    
